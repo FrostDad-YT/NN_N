e6b4619cf75045efd41024126b36ad22b62ba4b3
FrostDad <mr220360@gmail.com>
2020-10-31 23:49:58 +0700

    Update NN.py

--- NN.py
+++ NN.py
@@ -1,9 +1,12 @@
 import random
 import math
+import time
 
 n = 0  # количество эпох
 y = 0
 q = 0
+h = 0
+ER = 0
 MM = [0, 0]  # положение мыши
 MM1 = [0, 0]
 vector = []
@@ -31,104 +34,77 @@ def go(y):
     global MM
     if y == 1:
         if MM[1] - 1 >= 0:  # смотрим значение слева
-            print('1) MM в go:', MM, MM[1] - 1)
+            # print('1) MM в go:', MM, MM[1] - 1)
             return y
     elif y == 2:
         if MM[0] - 1 >= 0:  # смотрим значение сверху
-            print('2) MM в go:', MM, MM[0] - 1)
+            # print('2) MM в go:', MM, MM[0] - 1)
             return y
     elif y == 3:
         if MM[1] + 1 < N:  # смотрим значение справа
-            print('3) MM в go:', MM, MM[1] + 1)
+            # print('3) MM в go:', MM, MM[1] + 1)
             return y
     elif y == 4:
         if MM[0] + 1 < N:  # смотрим значение снизу
-            print('4) MM в go:', MM, MM[0] + 1)
+            # print('4) MM в go:', MM, MM[0] + 1)
             return y
     else:
-        print('return 0')
+        # print('return 0')
         return 0
 
 
 '''Функция принятия рандомного действия'''
-def Random():
-    global MM
-    print('рандом')
-    q = random.randint(1, 4)  # генерируем случайное действие
-    if q == 1:
-        if MM[1] - 1 >= 0:  # смотрим значение слева
-            return q
-    elif q == 2:
-        if MM[0] - 1 >= 0:  # смотрим значение сверху
-            return q
-    elif q == 3:
-        if MM[1] + 1 < N:  # смотрим значение справа
-            return q
+def Random() -> object:
+    global MM, h, q
+    q = random.randint(1, 4)
+    # print('рандом =', q)
+    h = go(q)
+    # print('h = go(q)', h)
+    if h in [1, 2, 3, 4]:
+        return h
     else:
-        if MM[0] + 1 < N:  # смотрим значение снизу
-            return q
-    return Random()
+        Random()
 
 
 def action():  # вычисляется оптимальный ход, выводит значение от 1 до 4, в зависимости от того куда стоит сделать ход
     global MM, MM1
+    # print('action')
     vector.clear()
     MM1 = [0, 0]  # временное положение мыши для вычислений
-    if go(1) != 0:  # смотрим значение слева
-        MM1 = MM
+    if go(1) in [1, 2, 3, 4]:  # смотрим значение слева
+        MM1 = MM[:]
         MM1[1] -= 1
+        # print('1) MM1 в action:', MM1)
         vector.append(matrix[int(MM1[0])][int(MM1[1])])  # в вектор vector первым значением запишем слева от мыши
     else:
         vector.append(0)
-    if go(2) != 0:  # смотрим значение сверху
-        MM1 = MM
+    if go(2) in [1, 2, 3, 4]:  # смотрим значение сверху
+        MM1 = MM[:]
         MM1[0] -= 1
         vector.append(matrix[int(MM1[0])][int(MM1[1])])  # в вектор vector вторым значением запишем сверху от мыши
     else:
         vector.append(0)
-    if go(3) != 0:  # смотрим значение справа
-        MM1 = MM
+    if go(3) in [1, 2, 3, 4]:  # смотрим значение справа
+
+        MM1 = MM[:]
         MM1[1] += 1
+        # print('3) MM1 в action:', MM1)
         vector.append(matrix[int(MM1[0])][int(MM1[1])])  # в вектор vector третьим значением запишем справа от мыши
     else:
         vector.append(0)
-    if go(4) != 0:  # смотрим значение снизу
-        MM1 = MM
+    if go(4) in [1, 2, 3, 4]:  # смотрим значение снизу
+        MM1 = MM[:]
         MM1[0] += 1
         vector.append(matrix[int(MM1[0])][int(MM1[1])])  # в вектор vector четвёртым значением запишем снизу от мыши
     else:
         vector.append(0)
     # if go(vector.index(max(vector))) != 0:
-    print('vector:', vector, max(vector), 1 + vector.index(max(vector)))
+    # print('vector:', vector, max(vector), 1 + vector.index(max(vector)))
     return 1 + vector.index(max(vector)) # так как список значений имеет вид 0-3, а действия в системе обозначены как 1-4
     # else:
 
 
-
-'''основной цикл просчета пути'''
-for i in range(5000):  # MM => [строка, столбец]
-    n = i
-    # while (MM[0] != N - 1 and MM[1] != N - 1):
-    #     pass
-    ER = exploration_rate(n)
-    print(MM)
-    print(ER)
-    if ER == 1:
-        MM[1] -= 1
-    elif ER == 2:
-        MM[0] -= 1
-    elif ER == 3:
-        MM[1] += 1
-    else:
-        MM[0] += 1
-
-    print(MM)
-    # print(matrix)
-    # for _ in matrix:
-    #     print(_)
-
-    print()
-
+def render():
     for i in range(N):
         for _ in range(N):
             if MM[0] == i and MM[1] == _:
@@ -136,6 +112,26 @@ for i in range(5000):  # MM => [строка, столбец]
             else:
                 print('#', end=' ')
         print()
+
+
+'''основной цикл просчета пути'''
+for i in range(50009):  # MM => [строка, столбец]
+    # time.sleep(0.1)
+    ER = exploration_rate(i)
+    # print(MM)
+    # print(ER)
+    if ER == 1:
+        MM[1] -= 1
+    elif ER == 2:
+        MM[0] -= 1
+    elif ER == 3:
+        MM[1] += 1
+    elif ER == 4:
+        MM[0] += 1
+    # print(MM)
+
+    # render()
+
 
     """опишем поощирения"""
     # пока не описал
